b2c4040b316d83966392b0787f8079d81b1a686f
Justin Nyilas <justin_nyilas@hotmail.com>
2023-06-05 18:18:05 +1000

    Begin configuring routes

--- server/app.ts
+++ server/app.ts
@@ -4,11 +4,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
+
+app.use('/api/users', require('./routes/userRoutes'))
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
