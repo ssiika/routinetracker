6a8771bff228a496ec5779bf33ad729006f5cb27
justinnyilas <justin.nyilas@nodescribe.com>
2023-07-12 14:02:49 +1000

    Add functionality of all record routes

--- server/controllers/recordController.ts
+++ server/controllers/recordController.ts
@@ -3,6 +3,7 @@ const Record = require('../models/recordModel');
 import express, {Request, Response} from 'express';
 
 interface RecordFormat {
+    _id: String,
     activity_id: String,
     day: Date,
     expected: Number,
@@ -10,12 +11,14 @@ interface RecordFormat {
 }
 
 const addRecord = asyncHandler(async (req: Request, res: Response) => {
-    const { activity_id, day, expected, actual } = req.body;
+    const { activity_id, day, expected} = req.body;
 
-    if (!activity_id || !day || !expected || !actual) {
+    if (!activity_id || !day || !expected) {
         res.status(400)
-        throw new Error('Request must have activity id, day, expected and actual parameters')
+        throw new Error('Request must have activity id, day and expected parameters')
     }
+
+    const actual = req.body.actual ? req.body.actual : 0
 
     const record = await Record.create({
         activity_id, 
@@ -28,15 +31,32 @@ const addRecord = asyncHandler(async (req: Request, res: Response) => {
 })
 
 const getRecords = asyncHandler(async (req: Request, res: Response) => {
-    res.status(200).json({message: `Get record ${req.params.id}`})
+    // Get all records of a user. Requires activity id
+
+    const records = await Record.find({"activity_id": req.params.id}) as Array<RecordFormat>
+
+    res.status(200).json(records)
 })
 
 const updateRecord = asyncHandler(async (req: Request, res: Response) => {
-    res.status(200).json({message: `Update record ${req.params.id}`})
+    // Update actual parameter
+
+    const { actual } = req.body
+
+    if (actual === undefined) {
+        res.status(400)
+        throw new Error('Request must have actual parameter')
+    }
+
+    const recordUpdate = await Record.updateOne({"_id": req.params.id}, {$set: {"actual": actual }});
+
+    res.status(200).json(recordUpdate)
 })
 
 const deleteRecord = asyncHandler(async (req: Request, res: Response) => {
-    res.status(200).json({message: `Delete record ${req.params.id}`})
+    const deletedResponse = await Record.deleteOne({"_id": req.params.id})
+
+    res.status(200).json(deletedResponse)
 })
 
 module.exports = {
