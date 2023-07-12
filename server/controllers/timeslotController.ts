39d6b59c0fa45bbeb67923923294a9c190ccf32a
justinnyilas <justin.nyilas@nodescribe.com>
2023-07-12 14:51:27 +1000

    Add functionality of all timeslot routes

--- server/controllers/timeslotController.ts
+++ server/controllers/timeslotController.ts
@@ -1,25 +1,51 @@
 const asyncHandler = require('express-async-handler');
+const Activity = require('../models/activityModel');
 import express, {Request, Response} from 'express';
 
+interface TimeslotFormat {
+    day: Number,
+    starttime: Number,
+    duration: Number
+}
+
 const addTimeslot = asyncHandler(async (req: Request, res: Response) => {
-        res.status(200).json({message: `Set timeslot ${req.params.id}`})
+    const { day, starttime, duration } = req.body
+        
+    if (day === undefined || starttime === undefined || duration === undefined) {
+        res.status(400)
+        throw new Error('Request must have day, starttime and duration parameters')
+    }
+
+    const timeslotObject = {
+        day, 
+        starttime,
+        duration
+    }
+
+    const timeslotUpdate = await Activity.updateOne({_id: req.params.id}, {$push: {"timeslots": timeslotObject }});
+
+    res.status(200).json(timeslotUpdate)
 })
 
 const getTimeslots = asyncHandler(async (req: Request, res: Response) => {
-    res.status(200).json({message: `Get timeslot ${req.params.id}`})
+    const activityObject = await Activity.findOne({_id: req.params.id})
+
+    res.status(200).json(activityObject.timeslots)
 })
 
-const updateTimeslot = asyncHandler(async (req: Request, res: Response) => {
-    res.status(200).json({message: `Update timeslot ${req.params.id}`})
-})
 
 const deleteTimeslot = asyncHandler(async (req: Request, res: Response) => {
-    res.status(200).json({message: `Delete timeslot ${req.params.id}`})
+    // id should have format `${activityid}-${timeslotid}`
+
+    const [activityid, timeslotid] = req.params.id.split('-')
+
+    const deletedResponse = await Activity.updateOne({"_id": activityid}, {$pull: {"timeslots": {_id: timeslotid}}})
+
+    res.status(200).json(deletedResponse)
 })
 
 module.exports = {
     addTimeslot,
     getTimeslots,
-    updateTimeslot,
     deleteTimeslot
 }
--- server/routes/timeslotRoutes.ts
+++ server/routes/timeslotRoutes.ts
@@ -4,14 +4,12 @@ const router = express.Router()
 const {
     addTimeslot,
     getTimeslots,
-    updateTimeslot,
     deleteTimeslot,
 } = require('../controllers/timeslotController')
 
 router.route('/:id')
     .post(addTimeslot)
     .get(getTimeslots)
-    .put(updateTimeslot)
     .delete(deleteTimeslot)
 
 module.exports = router
