c90ef038902b56be75cf4eb68efd6a5bbe663155
justinnyilas <justin.nyilas@nodescribe.com>
2023-08-30 23:39:26 +1000

    Add function which groups records into months, calculates total and average minutes per month

--- client/src/pages/History.tsx
+++ client/src/pages/History.tsx
@@ -10,6 +10,13 @@ import Spinner from '../components/Spinner';
 import { Record } from '../types';
 
 function History() {
+
+  interface MonthGroupOutput {
+    year: number,
+    // Month should be month array index
+    month: number,
+    total: number,
+  }
   
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
@@ -17,6 +24,9 @@ function History() {
   const {user} = useSelector((state: RootState) => state.auth);
   const {userRecordList, isLoading: recordLoading} = useSelector((state: RootState) => state.records)
   const {userActivityList, isLoading: activityLoading} = useSelector((state: RootState) => state.activities);
+
+  const months: [string, number][] = [['Jan', 31], ['Feb', 28], ['Mar', 31], ['Apr', 30], ['May', 31], ['Jun', 30], 
+  ['Jul', 31], ['Aug', 31], ['Sep', 30], ['Oct', 31], ['Nov', 30], ['Dec', 31]]
 
   function sortCompare( a: Record, b: Record ) {
     if ( a.day < b.day ){
@@ -28,9 +38,47 @@ function History() {
     return 0;
   }
 
-
   const [recordData, setRecordData] = useState<Record[] | null>(null)
 
+  const getValuesFromDate = (date: Date | string): {year: number, month: number} => {
+    // Sanitise dates
+    if (date instanceof Date) {
+      date = date.toLocaleDateString();
+    }
+
+    const year = parseInt(date.slice(0, 4))
+    const month = parseInt(date.slice(5, 7))
+    return {year, month};
+  }
+
+  const groupByMonth = (array: Record[]): MonthGroupOutput[] => {
+    
+    let outputArray: MonthGroupOutput[] = [];
+
+    // Loop through array, check if output contains relevant year and month, if not create new item to push to array
+    for (let i = 0; i < array.length; i++) {
+      const {year, month} = getValuesFromDate(array[i].day)
+      const groupExists = outputArray.findIndex((obj) => (obj.year === year) && (obj.month === month))
+      if (groupExists < 0) {
+        // Entry doesn't exist yet. Need to create new one
+        outputArray.push({
+          year,
+          month,
+          total: array[i].time
+        })
+      } else {
+        // Entry already exists. Must append time to total
+        outputArray[groupExists] = {
+          ...outputArray[groupExists],
+          total: outputArray[groupExists].total + array[i].time
+        }
+      }
+    }
+    
+
+    return outputArray;
+ 
+  }
 
   const onActivityChange = (e: SyntheticEvent) => {
     const target = e.target as typeof e.target & {
@@ -86,9 +134,12 @@ function History() {
                 {(function () {
                   let recordArray: JSX.Element[] = [];
                   const defaultArray = userRecordList.filter((record) => record.activity_id === userActivityList[0]?._id).sort(sortCompare)
-                  defaultArray.map((record) => {
+                  const sortedArray = groupByMonth(defaultArray)
+                  sortedArray.map((group) => {
+                    // Average to 2 decimal places
+                    const avg = Math.round((group.total / months[group.month][1])* 100) / 100
                     recordArray.push(
-                      <div key={record._id}>{`${record.time} on ${record.day}`}</div>
+                      <div key={`${group.year}-${group.month}`}>{`${group.year}-${months[group.month][0]}: total ${group.total} average ${avg} minutes`}</div>
                     )
                   })
                   return recordArray
