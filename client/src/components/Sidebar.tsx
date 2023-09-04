49f0d4034b5a4002e0ca75a179759737bd249e99
justinnyilas <justin.nyilas@nodescribe.com>
2023-09-04 17:33:56 +1000

    Add styling to header and sidebar

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,24 +1,44 @@
-import React from 'react'
+import React, { ReactHTMLElement } from 'react'
 import {Link} from 'react-router-dom';
 
 function Sidebar() {
+  const onEnter = (e: HTMLLIElement) => {
+    e.classList.add('sidebarSelected')
+  }
+
+  const onLeave = (e: HTMLLIElement) => {
+    e.classList.remove('sidebarSelected')
+  }
+
   return (
     <div className='sidebar'>
-        <li className='sidebarLink'>
+        <li className='sidebarLink' 
+            onMouseEnter={(e) => onEnter(e.target as HTMLLIElement)}
+            onMouseLeave={(e) => onLeave(e.target as HTMLLIElement)}
+        >
             <Link to='/schedule'>
                 Schedule
             </Link>
         </li>
-        <li className='sidebarLink'>
+        <li className='sidebarLink' 
+            onMouseEnter={(e) => onEnter(e.target as HTMLLIElement)}
+            onMouseLeave={(e) => onLeave(e.target as HTMLLIElement)}
+        >
             <Link to='/log'>
                 Log
             </Link>
         </li>   
-        <li className='sidebarLink'>
+        <li className='sidebarLink' 
+            onMouseEnter={(e) => onEnter(e.target as HTMLLIElement)}
+            onMouseLeave={(e) => onLeave(e.target as HTMLLIElement)}
+        >
             <Link to='/history'>
                 History
             </Link>
-        </li><li className='sidebarLink'>
+        </li><li className='sidebarLink' 
+            onMouseEnter={(e) => onEnter(e.target as HTMLLIElement)}
+            onMouseLeave={(e) => onLeave(e.target as HTMLLIElement)}
+        >
             <Link to='/guide'>
                 Guide
             </Link>
