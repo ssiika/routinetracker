92a8734eefc8766754b54c8ad40abf50271762e6
justinnyilas <justin.nyilas@nodescribe.com>
2023-09-16 23:33:51 +1000

    Fix bug with history graph being 1 month off, add styling to adjust for device screen height

--- client/src/components/Graph.tsx
+++ client/src/components/Graph.tsx
@@ -5,14 +5,12 @@ import 'chart.js/auto'
 
 function Graph({data, color, yAxisFilter}: {data: GroupedData[], color: string, yAxisFilter: string}) {
 
-    
-
     const months: [string, number][] = [['Jan', 31], ['Feb', 28], ['Mar', 31], ['Apr', 30], ['May', 31], ['Jun', 30], 
     ['Jul', 31], ['Aug', 31], ['Sep', 30], ['Oct', 31], ['Nov', 30], ['Dec', 31]]
 
     // Fix issue with activities with no data displaying data for activity1
     const graphData: GraphData[] = months.map((_month, index) => {
-        const monthData = data.find((data) => data.month === index) 
+        const monthData = data.find((data) => data.month - 1 === index) 
         if (monthData) {
             return {total: monthData.total, avg: monthData.avg}
         } else {
